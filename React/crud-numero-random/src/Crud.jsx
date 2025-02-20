062256e5893e4dfa60cc9a02aa5ab7b6d2a205e4
Sid-Ahmed-Souidi <sasoudi01@educarex.es>
2025-02-20 11:31:14 +0100

    REACT actualizacion

---
+++ React/crud-numero-random/src/Crud.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react'
+import { useEffect } from 'react';
+
+function Crud() {
+    const generarNumeroRandom = () => Math.round(Math.random() * 100) ;
+    const [Vector, setVector] = useState(Array.from({ length: 10 }, generarNumeroRandom));
+    console.log(Vector)
+
+    function agregarNumero(){
+    setVector([  generarNumeroRandom(),...Vector]);
+    }
+    function eliminar(index){
+        console.log(index)
+            const copiaV = [...Vector];
+            setVector(copiaV.splice(index,1))
+            setVector(copiaV)
+    }
+    function modificar(index){
+        //Esta manera si modificamos el numero 
+        const nuevoNumero = prompt("Teclea el nuevo número:", Vector[index]);
+        if (nuevoNumero !== null) {
+            const copiaV = [...Vector];
+            copiaV[index] = Number(nuevoNumero);
+            setVector(copiaV);
+        }
+        //de esta manera borramos directamente el numero
+        // const copiaV = [...Vector];
+        // setVector(copiaV.splice(index,1))
+        // const nuevoNumero = prompt("Teclea el nuevo número:", Vector[index]);
+        // setVector([  nuevoNumero,...Vector]);
+
+    }
+    function orderASC(){
+       const vectorAux= [...Vector].sort((a,b)=> a-b)
+    
+       setVector(vectorAux)
+    }
+    function orderDesc(){
+        const vectorAux= [...Vector].sort((a,b)=> b-a)
+    
+        setVector(vectorAux)
+    }
+return (
+    <div className='container'>
+        <button type="button" onClick={agregarNumero} className="btn btn-outline-primary">Añadir</button>
+        <button className='btn btn-danger' onClick={()=>orderASC()}>Order ASC</button>
+        <button className='btn btn-info' onClick={()=>orderDesc()}>Order DESC</button>
+
+   
+    <table className="table">
+        <thead>
+            <tr>
+                <th>Código</th>
+                <th>Número</th>
+                <th>Eliminar</th>
+                <th>Modificar</th>
+            </tr>
+        </thead>
+        <tbody>
+           {
+            Vector.map((item,index)=>(
+                <tr key={index}>
+                    <td>{index+1}</td>
+                    <td>{item}</td>
+                    <td><button className='btn btn-danger' onClick={()=>eliminar(index)} > Eliminar</button></td>
+                    <td><button className='btn btn-info' onClick={()=>modificar(index)}>Modificar</button></td>
+                    {
+                    }
+                </tr>
+            )
+           )
+            }
+        </tbody>
+    </table>  
+    </div>
+  )
+}
+
+export default Crud
